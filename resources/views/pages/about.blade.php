2ba174c0a121e38f325f112e51cc2e1fc05fd09b
Blake Renton <elblayko@gmail.com>
2019-05-05 20:59:26 +0000

    Modified front-end layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,10 +3,11 @@
 @section('title', 'Home')
 
 @section('content')
-  <section class="jumbotron text-center">
+  <section class="text-center">
     <div class="container">
-      <h1 class="jumbotron-heading">About this site</h1>
-      <p class="lead text-muted">This site is a demonstration of a blog-type Laravel application.</p>
+      <h1>About this site</h1>
+      <p class="lead text-muted">This site is a demonstration of a blog-type Laravel application.  You can customize it as you see fit.</p>
+      <i class="fab fa-github pr-1"></i><a href="https://github.com/elblayko/ExampleLaravelApplication">Download it on Github</a>
     </div>
   </section>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,10 +3,21 @@
 @section('title', 'Home')
 
 @section('content')
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1 class="jumbotron-heading">Welcome!</h1>
-      <p class="lead text-muted">This is the example Laravel application.</p>
-    </div>
+<section class="jumbotron text-center">
+  <div class="container">
+    <h1 class="jumbotron-heading">Welcome!</h1>
+    <p class="lead text-muted">This is the example Laravel application.</p>
+  </div>
+</section>
+<div class="row">
+  <aside class="col-md-4">
+    <h1>Sidebar Content</h1>
+    <p>This is the sidebar content.  You can feature anything that you would like, such as a link to a featured blog post, or another page on your website.</p>
+    <a href="/blog" class="btn btn-primary">Read more...</a>
+  </aside>
+  <section class="col-md-8">
+    <h1>Main Content</h1>
+    <p>The the most prominent content can be viewed here.  It can consist of the most important or relevant information that you would like visitors to see.</p>
   </section>
+</div>
 @endsection
