1f05a5d8e022d6a1ff27ae7b85570a02d8f40051
Blake Renton <elblayko@gmail.com>
2019-05-07 03:06:51 +0400

    Updated view post button arrangement

--- resources/views/blog/viewPost.blade.php
+++ resources/views/blog/viewPost.blade.php
@@ -3,27 +3,29 @@
 @section('title', 'View Post')
 
 @section('content')
-
+<div class="mb-3">
+    <a href="/blog" class="btn btn-primary btn-inline" role="button">Back</a>
+    <div class="btn-group float-right m-auto" aria-label="Action buttons">
+        <a class="btn btn-primary" href="/blog/{{$post->id}}/edit">Edit Post</a>
+        <form method="POST" action="/blog/{{$post->id}}">
+            @method('DELETE')
+            @csrf
+            <input type="submit" role="button" class="btn btn-danger" value="Delete Post">
+        </form>
+    </div>
+</div>
 
 <div class="card">
-    <div class="card">
-        <div class="card-header align-bottom">
-            <h3 class="d-inline-block">{{ $post->title }}</h3>
-            <form method="POST" class="d-inline-block float-right" action="/blog/{{$post->id}}">
-                @method('DELETE')
-                @csrf
-                <input type="submit" class="btn btn-danger float-right" value="Delete Post">
-            </form>
-            <a class="btn btn-primary d-inline-block float-right" href="/blog/{{$post->id}}/edit">Edit Post</a>
-        </div>
-        <div class="card-body mt-2">
-            <p class="card-text">{{ $post->body }}</p>
-            <hr />
-            Created at at {{ $post->created_at }}
-            @if ( $post->created_at != $post->updated_at )
-            last updated: {{ $post->updated_at }}
-            @endif
-        </div>
+    <div class="card-header">
+        <h3 class="d-inline-block">{{ $post->title }}</h3>
+    </div>
+    <div class="card-body mt-2">
+        <p class="card-text">{{ $post->body }}</p>
+        <hr />
+        Created at at {{ $post->created_at }}
+        @if ( $post->created_at != $post->updated_at )
+        last updated: {{ $post->updated_at }}
+        @endif
     </div>
 </div>
 @endsection
